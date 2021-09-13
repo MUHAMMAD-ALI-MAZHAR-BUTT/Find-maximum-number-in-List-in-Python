5aaa0a6cd31aacec6ec73d3f7b942469447026a2
Muhammad Ali Mazhar butt <57632737+MUHAMMAD-ALI-MAZHAR-BUTT@users.noreply.github.com>
2021-09-13 04:05:12 -0700

    Create Maximum_element_in_list.py

---
+++ Maximum_element_in_list.py
@@ -0,0 +1,5 @@
+print("********************* Program to Find largest number in List *****************************************\n\n")
+list_ = [1,2,3,4,5,6,7,8,9,0]
+print('List elements are : ', list_ ,end="\n\n")
+print("Maximum element in list is : ",max(list_))
+print("\n\n**************************************************************")
